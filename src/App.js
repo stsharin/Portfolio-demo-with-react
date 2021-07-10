78b0876859711c9e264a9a5c916156465f557885
Saraban Tasnim Sharin <stssharin@gmail.com>
2021-07-11 03:21:52 +0600

    added about

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import About from "./components/About/About";
+import Home from "./components/Home/Home";
+import NotFound from "./components/NotFound/NotFound";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hey There!</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/home">
+          <Home/>
+        </Route>
+        <Route path="/about">
+          <About/>
+        </Route>
+        <Route exact path="/">
+          <Home/>
+        </Route>
+        <Route path="*">
+          <NotFound/>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/About/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <div className="col-md-6">
+                <p>lorem ipsum dolor sit amet, consectetur adip</p>
+            </div>
+            <div className="col-md-6">
+                <p>lorem ipsum dolor sit amet, consectetur adip</p>
+            </div>
+        </div>
+    );
+};
+
+export default About;
