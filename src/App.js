477c066cbb89ac9897580ee4b09eef033d533ec6
Saraban Tasnim Sharin <stssharin@gmail.com>
2021-07-11 21:37:22 +0600

    added the background

--- src/App.js
+++ src/App.js
@@ -1,32 +1,12 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import About from "./components/About/About";
-import Home from "./components/Home/Home";
-import NotFound from "./components/NotFound/NotFound";
+import './App.css'
+import Header from "./components/Banner/Banner";
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/home">
-          <Home/>
-        </Route>
-        <Route path="/about">
-          <About/>
-        </Route>
-        <Route exact path="/">
-          <Home/>
-        </Route>
-        <Route path="*">
-          <NotFound/>
-        </Route>
-      </Switch>
-    </Router>
+    <div>
+      <Header/>
+    </div>
   );
 }
 
---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <header className="header">
+            
+        </header>
+    );
+};
+
+export default Banner;
